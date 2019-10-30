e9930e37fef9fd64e39a86fe414f04f9b24c655c
Bernardo Escoffié Torre <escoffie@think-comunicacion.com>
2019-10-30 17:17:34 -0600

    Prueba 01 con servicios (no terminado)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,57 @@
 let hamb = document.getElementById("hamburger");
 let menu = document.getElementById("main-menu");
 let links = document.querySelectorAll("#main-menu a");
+let servbtns = document.querySelectorAll("#servicios .servicio");
+let carousel = document.getElementById("carousel");
 
 hamb.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
 links.forEach(function(link){
-    console.log(link);
     link.addEventListener('click', () => {
         menu.classList.toggle('active');
     });
 });
+
+servbtns.forEach(function(btn){
+    btn.addEventListener('click', () => {
+        servbtns.forEach(function(el){
+            if(el != btn) {
+                el.classList.remove('active');
+            }
+        });
+        btn.classList.toggle('active');
+    });
+});
+
+$('#carousel').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth:true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
